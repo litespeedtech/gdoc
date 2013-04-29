11c60c5cc86c35c8e2f7da99bff426aed9afe05b
Lauren Song <lsong@litespeedtech.com>
2013-04-29 14:59:41 -0400

    m

--- genDoc_OLSWS.sh
+++ genDoc_OLSWS.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-rm -rf docs/*
+if [ -e docs ]; then
+    rm -rf docs/*
+else
+    mkdir docs
+fi
 
 cd scripts
 php gen_ows_help.php
 cd ..
 
 cp static/ows/*.html docs/
-cp -R static/css docs/css
-cp -R static/img docs/img
+cp -Rf static/css docs/
+cp -Rf static/img docs/
