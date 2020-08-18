12a8e4242bb42595352f985e8cf5d7cfa1160abe
laoxong <lao__xong@outlook.com>
2020-08-18 21:49:47 +0800

    Chinese

--- scripts/text_lang/zh-CN/common/BasicTerm.php
+++ scripts/text_lang/zh-CN/common/BasicTerm.php
@@ -2,21 +2,21 @@
 
 // definition of basic terms used. This file has to be placed under common with fixed file name
 
-$ref['ows_user_manual'] = 'Open LiteSpeed Web Server Users\' Manual';
+$ref['ows_user_manual'] = 'OpenLiteSpeed Users\' Manual';
 $ref['ws_user_manual'] = 'LiteSpeed Web Server Users\' Manual';
 $ref['lb_user_manual'] = 'LiteSpeed Web ADC Users\' Manual';
 $ref['copyright'] = 'Copyright';
 $ref['all_rights_reserved'] = 'All rights reserved.';
 
 $ref['syntax_bool'] = 'Select from radio box' ;
-$ref['syntax_path1'] = '绝对路径' ;
+$ref['syntax_path1'] = 'Absolute path' ;
 $ref['syntax_path2'] = 'An absolute path or a relative path to $SERVER_ROOT.' ;
 $ref['syntax_file2'] = 'Filename which can be an absolute path or a relative path to $SERVER_ROOT.' ;
 $ref['syntax_file3'] = 'Filename which can be an absolute path or a relative path to $SERVER_ROOT, $VH_ROOT.' ;
 $ref['syntax_path3'] = 'A path which can be absolute, relative to $SERVER_ROOT, or relative to $VH_ROOT.' ;
 $ref['syntax_select'] = 'Select from drop down list' ;
 $ref['syntax_checkbox'] = 'Select from checkbox' ;
-$ref['syntax_uint'] = '无符号整数' ;
+$ref['syntax_uint'] = 'Integer number' ;
 
 $ref['label_description'] = 'Description';
 $ref['label_syntax'] = 'Syntax';
@@ -27,3 +27,4 @@ $ref['label_seealso'] = 'See Also';
 $ref['label_toc'] = 'Table of Contents';
 
 $ref['apply_1'] = 'Reinstall required.';
+
